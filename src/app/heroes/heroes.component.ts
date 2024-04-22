9bf02a01058f3cb1b1614783adb4a5d26bd8b755
Bart <bwj.kempen@student.avans.nl>
2024-04-22 19:36:03 +0200

    add service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core'
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common'
 import { FormsModule } from '@angular/forms'
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service'
 import { Hero } from '../hero'
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component'
+import { MessageService } from '../message.service'
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,25 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component'
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes = HEROES
+  heroes: Hero[] = []
+
   selectedHero?: Hero
+
+  constructor (
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit (): void {
+    this.getHeroes()
+  }
+
   onSelect (hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
+  }
+
+  getHeroes (): void {
+    this.heroService.getHeroes().subscribe(heroes => (this.heroes = heroes))
   }
 }
